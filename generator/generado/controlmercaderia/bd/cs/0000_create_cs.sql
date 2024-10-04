355ed482422d457394daad83faec2d69594e6636
Fabian <affa08029@gamil.com>
2024-10-03 22:15:46 -0400

    eliminacion de tablas

--- generator/generado/controlmercaderia/bd/cs/0000_create_cs.sql
+++ generator/generado/controlmercaderia/bd/cs/0000_create_cs.sql
@@ -284,6 +284,39 @@ CREATE SEQUENCE CS_USUARIO_SEQ
 	increment 1 minvalue 1 maxvalue 9223372036854775807 start 100 cache 1;
 
 
+/* TABLE CS_UTILIZADOR */
+CREATE TABLE CS_UTILIZADOR
+(
+	id_utilizador numeric(9,0) NOT NULL,
+	usuario character varying(60) NOT NULL,
+	contrasenha character varying(60) NOT NULL,
+	id_cargo numeric(9,0) NOT NULL,
+	id_empleado numeric(9,0) NOT NULL,
+	imagen_portada character varying(500) DEFAULT '[]',
+	activo boolean NOT NULL DEFAULT 'true',
+
+	/*campos genericos auditoria*/
+	zk_usr_crea character varying(60) not null,
+	zk_usr_modi character varying(60),
+	zk_fec_crea timestamp without time zone not null,
+	zk_ult_modi timestamp without time zone not null,
+	zk_empresa_core character varying(30) not null,
+	zk_cuenta character varying(60) not null,
+	zk_eliminado boolean not null,
+	zk_fec_elim timestamp without time zone,
+	zk_uuid character varying(100) not null
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE CS_UTILIZADOR 
+	ADD CONSTRAINT "CS_UTILIZADOR-id_utilizador_pk" PRIMARY KEY (id_utilizador);
+
+CREATE SEQUENCE CS_UTILIZADOR_SEQ
+	increment 1 minvalue 1 maxvalue 9223372036854775807 start 100 cache 1;
+
+
 /* CONSTRAINT CS_DETALLE_PEDIDO */
 ALTER TABLE CS_DETALLE_PEDIDO 
 	ADD CONSTRAINT "CS_DETALLE_PEDIDO-id_pedido_fk"  FOREIGN KEY (id_pedido)
@@ -392,6 +425,31 @@ ALTER TABLE CS_USUARIO
 
 ALTER TABLE CS_USUARIO 
 	ADD CONSTRAINT "CS_USUARIO-id_empleado_fk"  FOREIGN KEY (id_empleado)
+	REFERENCES cs_empleado (id_empleado) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* CONSTRAINT CS_UTILIZADOR */
+ALTER TABLE CS_UTILIZADOR 
+	ADD CONSTRAINT "CS_UTILIZADOR-id_cargo_fk"  FOREIGN KEY (id_cargo)
+	REFERENCES cs_cargo (id_cargo) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+ALTER TABLE CS_UTILIZADOR 
+	ADD CONSTRAINT "CS_UTILIZADOR-id_empleado_fk"  FOREIGN KEY (id_empleado)
 	REFERENCES cs_empleado (id_empleado) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 
